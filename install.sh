b21ef73cd560b77b9b28597876930fce49299644
Ozan Kukul <ozankukul.cs@gmail.com>
2022-11-19 12:01:42 +0300

    initial commit

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+USERHOME=$HOME
+
+sudo dnf update -y
+
+#fonts
+fontdir="$USERHOME/.local/share/fonts/"
+mkdir -p $fontdir && cp -r ./home/.local/share/fonts/* $fontdir
+
+# cinnamond desktop configuration
+cp -r ./home/.cinnamon $USERHOME
+cp -r ./home/.config/* $USERHOME/.config/
+cp -r ./home/.themes/* $USERHOME/.themes/
+sudo cp ./cinnamon.css /usr/share/themes/Mint-Y-Dark/cinnamon/cinnamon.css
+dconf load / < my-cinnamon.dconf
+
+# my programs
+cp -r ./home/myPrograms $USERHOME
+
+# services
+sudo cp ./services/kmonad.service /etc/systemd/system/kmonad.service
+sudo systemctl enable kmonad.service
+sudo systemctl start kmonad.service
+
+# rpm fusion
+sudo dnf -y install dnf-plugins-core
+sudo dnf install -y \
+https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
+https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo 
+sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+# rpm fusion vscode
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+# install programs
+sudo dnf install -y git code flatpak steam
+sudo flatpak install flatseal telegram discord zoom
