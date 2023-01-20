3c4dea839b2922fae4bb31b57747f495ee69d450
Ozan Kukul <ozankukul.cs@gmail.com>
2023-01-20 14:50:51 +0300

    Update install.sh, add google-chrome

--- install.sh
+++ install.sh
@@ -59,10 +59,13 @@ https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -
 https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo 
 sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-# rpm fusion vscode
+# enable vscode
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+# enable google chrome
+sudo dnf install -y fedora-workstation-repositories
+sudo dnf config-manager --set-enabled google-chrome
 
 # install programs
-sudo dnf install -y openh264 htop bat git code steam telegram flatpak
+sudo dnf install -y openh264 htop bat git code google-chrome-stable steam telegram flatpak
 sudo flatpak install -y flathub flatseal discord zoom onlyoffice
