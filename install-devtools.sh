2d75b085bcbe50903cd699934fbac3d1acdd8d06
Ozan Kukul <ozankukul.cs@gmail.com>
2023-01-21 14:23:24 +0300

    Update install-devtools.sh, add miniconda

--- install-devtools.sh
+++ install-devtools.sh
@@ -6,3 +6,12 @@ sudo dnf install -y podman nginx rabbitmq-server
 sudo dnf install -y rust golang nodejs dotnet nuget
 sudo npm install -g pnpm
 pnpm setup # && source $HOME/.bashrc
+
+# install miniconda
+miniconda="https://repo.anaconda.com/miniconda/Miniconda3-py310_22.11.1-1-Linux-x86_64.sh"
+wget -O miniconda-installer.sh $miniconda
+chmod +x miniconda-installer.sh
+./miniconda-installer.sh -b -p $HOME/myPrograms/miniconda3
+$HOME/myPrograms/miniconda3/bin/conda init
+source $HOME/.bashrc
+conda config --set auto_activate_base false
