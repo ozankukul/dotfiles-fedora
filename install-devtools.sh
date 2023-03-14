263cfaca0ba7b083c12044f52ba2d828f7d7cde8
ozankukul <79106017+ozankukul@users.noreply.github.com>
2023-03-14 21:11:41 +0300

    Update install-devtools.sh, replace rust w/ rustup

--- install-devtools.sh
+++ install-devtools.sh
@@ -3,10 +3,11 @@
 # install devtools
 sudo dnf install -y @virtualization hugo
 sudo dnf install -y podman nginx rabbitmq-server
-sudo dnf install -y rust golang nodejs dotnet nuget
+sudo dnf install -y golang nodejs dotnet nuget
 sudo npm install -g pnpm
 pnpm setup && source $HOME/.bashrc
 pnpm install -g typescript
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # install miniconda
 miniconda="https://repo.anaconda.com/miniconda/Miniconda3-py310_22.11.1-1-Linux-x86_64.sh"
