ab56a4cde5bb09c78ec32c581bf8890093d67662
ozankukul <79106017+ozankukul@users.noreply.github.com>
2023-03-14 21:18:48 +0300

    Update imade-video-editing.sh, remove drawing

--- install-image-video-editing.sh
+++ install-image-video-editing.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # install video and image programs
-sudo dnf install -y obs-studio handbrake-gui kdenlive celluloid krita drawing inkscape # obs>settings>output:ffmpeg-mp4-nvenc
+sudo dnf install -y obs-studio handbrake-gui kdenlive celluloid krita inkscape # obs>settings>output:ffmpeg-mp4-nvenc
