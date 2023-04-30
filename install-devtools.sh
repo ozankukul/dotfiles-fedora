c9b2e507edb79fe9268240e7e627f91917c82d6a
ozankukul <79106017+ozankukul@users.noreply.github.com>
2023-04-30 18:29:47 +0300

    Add python3.7, remove miniconda

--- install-devtools.sh
+++ install-devtools.sh
@@ -9,11 +9,14 @@ pnpm setup && source $HOME/.bashrc
 pnpm install -g typescript
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
+# install python3.7
+dnf install -y python3.7
+python3.7 -m ensurepip
 # install miniconda
-miniconda="https://repo.anaconda.com/miniconda/Miniconda3-py310_22.11.1-1-Linux-x86_64.sh"
-wget -O miniconda-installer.sh $miniconda
-chmod +x miniconda-installer.sh
-./miniconda-installer.sh -b -p $HOME/myPrograms/miniconda3
-$HOME/myPrograms/miniconda3/bin/conda init
-source $HOME/.bashrc
-conda config --set auto_activate_base false
+## miniconda="https://repo.anaconda.com/miniconda/Miniconda3-py310_22.11.1-1-Linux-x86_64.sh"
+## wget -O miniconda-installer.sh $miniconda
+## chmod +x miniconda-installer.sh
+## ./miniconda-installer.sh -b -p $HOME/myPrograms/miniconda3
+## $HOME/myPrograms/miniconda3/bin/conda init
+## source $HOME/.bashrc
+## conda config --set auto_activate_base false
